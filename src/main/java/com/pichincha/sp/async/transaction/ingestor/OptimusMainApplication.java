58adba61372c8708929f256a6eadd34398471077
Santiago Yacelga <esyacelga@hotmail.com>
2025-05-14 16:16:53 -0500

    First commit

---
+++ src/main/java/com/pichincha/sp/async/transaction/ingestor/OptimusMainApplication.java
@@ -0,0 +1,33 @@
+package com.pichincha.sp.async.transaction.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Arquitectura tradicional para el recurso mtj-msa-sp-async-transaction-ingestor extendiendo
+ * de la interfaz generada.
+<br/>
+ * <b>Class</b>: PichinchaOptimusMainApplication<br/>
+ * <b>Copyright</b>: &copy; 2025 Banco Pichincha<br/>
+ *
+ * @author Banco Pichincha <br/>
+ * <u>Developed by</u>: <br/>
+ * <ul>
+*
+<li>santiago yacelga</li>
+*
+</ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+*
+<li>May 14, 2025 Creaci&oacute;n de Clase.</li>
+*
+</ul>
+ * @version 1.0
+ */
+@SpringBootApplication
+public class OptimusMainApplication {
+
+  public static void main(String[] args) {
+        SpringApplication.run(OptimusMainApplication.class, args);
+    }
+}
