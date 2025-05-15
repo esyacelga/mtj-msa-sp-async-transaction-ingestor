d0703a5615ad10c1ed51b11d48510362fa65445f
Santiago Yacelga <esyacelga@hotmail.com>
2025-05-15 15:38:02 -0500

    Pruebas unitarias

--- src/main/java/com/pichincha/sp/async/transaction/ingestor/infrastructure/adapter/DefaultTransactionProcessor.java
+++ src/main/java/com/pichincha/sp/async/transaction/ingestor/infrastructure/adapter/DefaultTransactionProcessor.java
@@ -3,9 +3,11 @@ package com.pichincha.sp.async.transaction.ingestor.infrastructure.adapter;
 import com.pichincha.sp.async.transaction.ingestor.application.output.port.TransactionProcessor;
 import com.pichincha.sp.async.transaction.ingestor.domain.entities.Transaction;
 import com.pichincha.sp.async.transaction.ingestor.infrastructure.output.repository.TransactionRepository;
+import com.pichincha.sp.event.domain.model.event.EventMessageAvro;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,7 +17,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class DefaultTransactionProcessor implements TransactionProcessor {
     private final TransactionRepository transactionRepository;
-    /// private final KafkaTemplate<String, SpecificRecord> kafkaTemplate;
+    private final KafkaTemplate<String, EventMessageAvro> kafkaTemplate;
 
     @Value("${app.kafka.topic}")
     private String topic;
@@ -26,9 +28,18 @@ public class DefaultTransactionProcessor implements TransactionProcessor {
         Transaction transaction = transactionRepository.findById(transactionId)
                 .orElseThrow(() -> new IllegalStateException("Transaction not found: " + transactionId));
 
-        /// kafkaTemplate.send(topic, transaction);
+        EventMessageAvro eventMessage = new EventMessageAvro();
+        kafkaTemplate.send(topic, eventMessage);
         log.info("Sent transaction {} to Kafka topic {}", transactionId, topic);
     }
+/*    @Override
+    public void processTransaction(UUID transactionId) {
+        Transaction transaction = transactionRepository.findById(transactionId)
+                .orElseThrow(() -> new IllegalStateException("Transaction not found: " + transactionId));
+
+        /// kafkaTemplate.send(topic, transaction);
+        log.info("Sent transaction {} to Kafka topic {}", transactionId, topic);
+    }*/
 
     /*@Override
     public void processTransaction(UUID transactionId) {
--- src/main/java/com/pichincha/sp/async/transaction/ingestor/infrastructure/config/KafkaConfig.java
+++ src/main/java/com/pichincha/sp/async/transaction/ingestor/infrastructure/config/KafkaConfig.java
@@ -1,17 +1,40 @@
 package com.pichincha.sp.async.transaction.ingestor.infrastructure.config;
 
-import com.pichincha.sp.async.transaction.ingestor.domain.entities.Transaction;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
-    public KafkaTemplate<String, Transaction> kafkaTemplate(ProducerFactory<String, Transaction> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        configProps.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081"); // ajusta según tu entorno
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
